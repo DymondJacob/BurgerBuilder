a9e67e9c110eaca1d3c3a020414805ab0f0c2e84
JacobDymond <devdymond@gmail.com>
2018-11-26 18:59:18 -0600

    Started Burger
    Started the architecture of the project build, state for ingredients has been added. Burger is successfully being rendered to the DOM.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Auxx from "../../hoc/Auxx";
+import "./Layout.css";
+
+const layout = props => (
+  <Auxx>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className="Content">{props.children}</main>
+  </Auxx>
+);
+
+export default layout;
---
+++ src/containers/BurgerBuild/BurgerBuild.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Auxx from "../../hoc/Auxx";
+import Burger from "../../components/Burger/Burger";
+
+class BurgerBuild extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
+  render() {
+    return (
+      <Auxx>
+        <Burger ingredients={this.state.ingredients} />
+        <div>Build Controls</div>
+      </Auxx>
+    );
+  }
+}
+
+export default BurgerBuild;
